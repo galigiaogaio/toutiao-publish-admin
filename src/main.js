b4c1e1329ff281be678d68111b0464c557a9592a
galigiaogiao <1225890480@qq.com>
2020-12-06 18:45:13 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+/* 项目启动入口 */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
+/* 创建Vue 根实例 */
+/* 把router配置到根实例中 */
+// 通过render方法把APP根组件渲染到  #app入口节点（入口节点就是public里的index.html）中
 new Vue({
   router,
   render: h => h(App)
